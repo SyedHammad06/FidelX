8974ada7bbbcf9a058d2b1079f719e3fe86c8a99
SyedHammad06 <58735133+SyedHammad06@users.noreply.github.com>
2021-04-21 15:25:49 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import LandingPage from './Components/LandingPage/LandingPage';
+import './App.scss';
+
+function App() {
+  return (
+    <div className='App'>
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
